d068a419f43fdab7620a971ec6757a6cd8908e20
Sujana Bhandari <sujana6589@gmail.com>
2022-11-05 13:55:34 +0100

    Verify token and authenticate user

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,12 @@ var express = require('express');
 var usersRouter = express.Router();
 
 const {registerUser, loginUser, getUser} = require("../controllers/Auth");
+const 
+    verifyToken
+   = require("../middlewares/verifyToken");
 
 /* GET users listing. */
 usersRouter.route("/signup").post(registerUser);
 usersRouter.post('/signin', loginUser);
-usersRouter.get('/me', getUser);
+usersRouter.get('/me', verifyToken,getUser);
 module.exports = usersRouter;
