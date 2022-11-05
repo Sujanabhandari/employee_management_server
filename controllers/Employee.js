efea60fc2f41bb7d91c0a0f5a50d481b1dd2c683
Sujana Bhandari <sujana6589@gmail.com>
2022-11-05 15:24:56 +0100

    Add, Update, Delete employee functionality

---
+++ controllers/Employee.js
@@ -0,0 +1,82 @@
+const User = require("../models/Users");
+const { asyncHandler } = require("../utils/asyncHandler.js");
+
+const getAllEmployee = asyncHandler(async (req, res, next) => {
+  const condition = req.query;
+
+  const allEmployee = await User.find(condition);
+  if (!allEmployee.length)
+    return res
+      .status(400)
+      .send(
+        "There are no existing employee.Please Create a new Employee."
+      );
+
+  res.status(200).send(allEmployee);
+}
+);
+
+const addNewEmployee = asyncHandler(async (req, res, next) => {
+  const { body, userId } = req;
+  console.log("Hello users");
+
+  console.log(userId);
+  const newEmployee = await User.create({ ...body, employee: userId });
+  res.status(201).send(newEmployee);
+});
+
+const getSingleEmployee = asyncHandler(async (req, res, next) => {
+  const {
+    params: { id }
+  } = req;
+  const employee = await User.findById(id);
+  if (!employee)
+    return res
+      .status(400)
+      .send(
+        `There are no existing employee with this id${id}`
+      );
+  res.send(employee);
+});
+
+const updateEmployee = asyncHandler(async (req, res, next) => {
+  const {
+    userId,
+    body,
+    params: { id }
+  } = req;
+  const foundEmployee = await User.findById(id);
+  if (!foundEmployee)
+    return res
+      .status(400)
+      .send(
+        `Employee with this ${id} doesn't exist`
+      );
+  const updateEmployee = await User.findOneAndUpdate({ _id: id }, body, { new: true });
+
+  res.send(updateEmployee);
+});
+
+const deletePost = asyncHandler(async (req, res, next) => {
+  const {
+    userId,
+    params: { id }
+  } = req;
+  const found = await User.findById(id);
+  if (!found)
+    return res
+      .status(400)
+      .send(
+        `Employee with this ${id} doesn't exist`
+      );
+  await User.deleteOne({ _id: id });
+  res.send({ success: `Employee with id of ${id} was deleted` });
+});
+
+module.exports = {
+  getAllEmployee,
+  addNewEmployee,
+  getSingleEmployee,
+  updateEmployee,
+  deletePost
+};
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,15 @@ var express = require('express');
 var usersRouter = express.Router();
 
 const {registerUser, loginUser, getUser} = require("../controllers/Auth");
+const {getAllEmployee, addNewEmployee,getSingleEmployee,updateEmployee,deletePost} = require("../controllers/Employee");
 const 
     verifyToken
    = require("../middlewares/verifyToken");
 
 /* GET users listing. */
+usersRouter.route("/").get(getAllEmployee);
+usersRouter.route("/:id").get(getSingleEmployee).put(verifyToken, updateEmployee).delete(verifyToken, deletePost);
+usersRouter.route("/addemployee").post(verifyToken, addNewEmployee);
 usersRouter.route("/signup").post(registerUser);
 usersRouter.post('/signin', loginUser);
 usersRouter.get('/me', verifyToken,getUser);
