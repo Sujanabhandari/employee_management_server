a68173848d28602ecf1cb465481febe8d081579e
Sujana Bhandari <sujana6589@gmail.com>
2022-11-05 09:35:04 +0100

    Register and Login employee

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var usersRouter = express.Router();
 
-const {registerUser} = require("../controllers/Auth");
+const {registerUser, loginUser, getUser} = require("../controllers/Auth");
 
 /* GET users listing. */
 usersRouter.route("/signup").post(registerUser);
+usersRouter.post('/signin', loginUser);
+usersRouter.get('/me', getUser);
 module.exports = usersRouter;
