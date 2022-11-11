ffb03188354fc2ecf9723dbe86b7486ea688528b
Sujana Bhandari <sujana6589@gmail.com>
2022-11-11 03:16:16 +0100

    Refactor code

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import express from 'express';
 let usersRouter = express.Router();
 
 import { registerUser, loginUser, getUser } from '../controllers/Auth.js';
-import { getAllEmployee, addEmployees, getSingleEmployee,updateEmployee,deletePost } from '../controllers/Employee.js';
+import { getAllEmployee, addEmployees, getSingleEmployee,updateEmployee,deleteEmployee } from '../controllers/Employee.js';
 import verifyToken from '../middlewares/verifyToken.js';
 
 /* GET users listing. */
@@ -11,6 +11,6 @@ usersRouter.route("/users").get(verifyToken, getAllEmployee).post(verifyToken, a
 usersRouter.route("/signup").post(registerUser);
 usersRouter.post('/signin', loginUser);
 usersRouter.get('/users/me', verifyToken, getUser);
-usersRouter.route("/users/:id").get(verifyToken, getSingleEmployee).put(verifyToken, updateEmployee).delete(verifyToken, deletePost);
+usersRouter.route("/users/:id").get(verifyToken, getSingleEmployee).put(verifyToken, updateEmployee).delete(verifyToken, deleteEmployee);
 
 export default usersRouter;
